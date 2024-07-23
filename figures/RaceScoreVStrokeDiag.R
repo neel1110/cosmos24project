da0ad78907530b6b6c97f987ef21ed5b8965d84f
Haylie Brzozowski <hayliebrzozowski@Haylies-MacBook-Air.local>
2024-07-22 20:41:22 -0700

    hello

--- figures/RaceScoreVStrokeDiag.R
+++ figures/RaceScoreVStrokeDiag.R
@@ -1,3 +1,15 @@
-strokeData <- read.csv("data/Stroke.csv")
-head(strokeData)
+library(ggplot2)
+library(tidyverse)
+stroke_data <- read.csv("data/Stroke.csv")
+head(stroke_data)
+
+
+stroke_data <- mutate(stroke_data, RACE.score = as.factor(RACE.score))
+
+
+ggplot(data = stroke_data,
+       aes(x = RACE.score,
+           fill = Outcome)) +
+  geom_bar(position = "dodge")
+
 
